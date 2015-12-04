7f49bd46364f5a889b8b488370d77222ebe8826a
mkhsor <smakhlouf9@gmail.com>
2015-12-04 14:44:11 +0100

    _roleCard

---
+++ Pandemic-PCSS/Assets/_Scripts/_roleCard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _roleCard : Card
+{
+
+    public enum roleType { MEDIC, DISPATCHER, QURANTINE_SPECIALIST, CONTINGENCY, RESEARCHER, SCIENTIST, OPERATIONS_EXPERT };
+
+    public roleType role;
+
+    // Use this for initialization
+    void Start()
+    {
+        /*for (int i = 0; i < 7; i++)
+        {
+            if(transform.name = roleCard.)
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
