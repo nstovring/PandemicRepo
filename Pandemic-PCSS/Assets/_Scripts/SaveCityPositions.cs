e3ba8db0099dc6bbf6c05b72a33dfe4ef7bb2334
mkhsor <smakhlouf9@gmail.com>
2015-12-04 14:51:54 +0100

    SaveCityPosition

---
+++ Pandemic-PCSS/Assets/_Scripts/SaveCityPositions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SaveCityPositions : MonoBehaviour
+{
+    public bool updateGizmos;
+
+    public Vector2[] positions = new Vector2[48];
+
+    void Update()
+    {
+        if (updateGizmos)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = transform.GetChild(i).transform.position;
+                transform.GetChild(i).transform.name = GameManager.cityNames[i];
+            }
+            updateGizmos = false;
+        }
+    }
+}
